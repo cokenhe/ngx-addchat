5db28d82ebe80ebceea2882f00ed8bdc1ab029c0
cokenhe <cokenhe@gmail.com>
2018-03-03 15:21:41 +0800

    added feature: close fb iframe will hide <chat-provider> now

--- src/ngx-addchat/container/provider.component.ts
+++ src/ngx-addchat/container/provider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
@@ -8,11 +8,13 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class ProviderComponent implements OnInit {
 
-  @Input() phoneNumber?: string
+  @Input() phoneNumber: string
 
-  @Input() fbPageName?: string
+  @Input() fbPageName: string
 
-  public fbIframeUrl?: string
+  @Output() onClosed = new EventEmitter<boolean>()
+
+  public fbIframeUrl: string
 
   public showFbiFrame = false;
 
@@ -21,11 +23,9 @@ export class ProviderComponent implements OnInit {
   constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
-    console.log(this.fbPageName)
     if (this.fbPageName) {
       this.fbIframeUrl = `https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F${this.fbPageName}%2F&tabs=messages&width=300&height=300&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false`
       this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.fbIframeUrl)
     }
-    console.log(this.fbIframeUrl)
   }
 }
